618b8db216f584cb1cd5af087746cd8d67a2d613
Gromer <rus.solovey.09@mail.ru>
2025-01-20 15:06:04 +0300

    update

--- script.js
+++ script.js
@@ -3,22 +3,25 @@ let deleteBtns = document.getElementsByClassName('deleteBtn');
 
 let inputText = document.getElementById("input")
 
-for (var i = 0; i < deleteBtns.length; i++) {
-  deleteBtns[i].addEventListener('click', function() {
-    this.parentNode.remove();
-  });
-}
-
 function addNote() {
   let notes = document.querySelector("#note-list");
   let note = document.createElement('li');
+  let deleteBtn = document.createElement('button');
   let input = document.getElementById('input')
   if (input.value != '')
   {
     note.classList.add('note');
     note.innerHTML = input.value;
     notes.appendChild(note);
+    deleteBtn.classList.add('deleteBtn');
+    deleteBtn.innerHTML = "X";
+    deleteBtns = document.getElementsByClassName('deleteBtn');
+    note.appendChild(deleteBtn);
     input.value = '';
   }
   input.value = '';
+}
+
+function deleteNote(el){
+  el.parentNode.remove();
 }
