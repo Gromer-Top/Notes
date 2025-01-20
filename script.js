c79530a78cfb87160f2036e6d6f718ae908009f6
Gromer <rus.solovey.09@mail.ru>
2025-01-20 11:31:17 +0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-var deleteBtns = document.getElementsByClassName('deleteBtn');
+//Массив кнопок удаления
+let deleteBtns = document.getElementsByClassName('deleteBtn');
+
+let inputText = document.getElementById("input")
 
 for (var i = 0; i < deleteBtns.length; i++) {
   deleteBtns[i].addEventListener('click', function() {
     this.parentNode.remove();
   });
+}
+
+function sendMessage() {
+  let notes = document.querySelector("notes");
+  let note = document.createElement('DIV');
+  let input = document.getElementById('input')
+  if (input.value != '')
+  {
+    notes.classList.add('note');
+    note.innerHTML = input.value;
+    chat.appendChild(note);
+    input.value = '';
+  }
+  input.value = '';
 }
