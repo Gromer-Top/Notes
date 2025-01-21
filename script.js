c2b36bc3a69cfc185f8d833eb1a6533345a2c35a
Gromer <rus.solovey.09@mail.ru>
2025-01-21 10:12:39 +0300

    update

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 let inputText = document.getElementById("input")
 
-function addNote() {
+let notesList = [];
+if(localStorage.getItem("1") != null)
+{
+  notesList.push(localStorage.getItem("1"));
+  addNote(notesList[0]);
+}
+
+function addNote(text) {
   let notes = document.querySelector("#note-list");
   let note = document.createElement('li');
   let deleteBtn = document.createElement('button');
   let input = document.getElementById('input')
+  if(text!= null){
+    note.classList.add('note');
+    note.innerHTML = text;
+    notes.appendChild(note);
+    deleteBtn.classList.add('deleteBtn');
+    deleteBtn.innerHTML = "X";
+    note.appendChild(deleteBtn);
+    deleteBtn.onclick = deleteNote;
+  }
   if (input.value != '')
   {
     note.classList.add('note');
     note.innerHTML = input.value;
     notes.appendChild(note);
     deleteBtn.classList.add('deleteBtn');
-    deleteBtn.innerHTML = "X";
     note.appendChild(deleteBtn);
     deleteBtn.onclick = deleteNote;
     input.value = '';
+    localStorage.setItem("1", note.innerText);
+    notesList.push(localStorage.getItem("1"));
+    console.log(notesList);
   }
   input.value = '';
 }
